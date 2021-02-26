22f1ff25d4786f400fdc217be1c885c3ec2eaa1e
Viking <wiktor.przadka.1@gmail.com>
2021-02-26 15:56:11 +0100

    initial commit

---
+++ src/main/java/jwzp/wp/VetApp/VetAppApplication.java
@@ -0,0 +1,13 @@
+package jwzp.wp.VetApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAppApplication.class, args);
+	}
+
+}
