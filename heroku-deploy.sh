a849ad6c6b1ec6cb97aea11b39d999502c5e9b57
wiktor.przadka.1 <wiktor.przadka.1@gmail.com>
2021-03-18 23:30:40 +0100

    move deploy script to file

---
+++ heroku-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+IMAGE_ID=$(docker inspect registry.heroku.com/$1/web --format={{.Id}})
+curl \
+  -n \
+  -X PATCH https://api.heroku.com/apps/$1/formation \
+  -d '{"updates":[{"type":"web", "docker_image":"'"$IMAGE_ID"'"}]}' \
+  -H "Content-Type:application/json" \
+  -H "Accept:application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization:Bearer $2"
