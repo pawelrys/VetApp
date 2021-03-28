96149f003aca2bc8abd9d9ced1f7abd1e597c90a
Viking <wiktor.przadka.1@gmail.com>
2021-03-28 18:18:56 +0200

    make app able to schedule tasks

--- src/main/java/jwzp/wp/VetApp/controller/api/VisitsController.java
+++ src/main/java/jwzp/wp/VetApp/controller/api/VisitsController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @RequestMapping(path="/api/visits")
 @RestController
+@Component
 public class VisitsController {
 
     private final VisitsService visitsService;
