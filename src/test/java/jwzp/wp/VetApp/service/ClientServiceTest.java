d5f559c12783139723e797617abc1972d471b214
wiktor.przadka.1 <wiktor.przadka.1@gmail.com>
2021-04-18 21:36:33 +0200

    test add client method

---
+++ src/test/java/jwzp/wp/VetApp/service/ClientServiceTest.java
@@ -0,0 +1,31 @@
+package jwzp.wp.VetApp.service;
+
+import jwzp.wp.VetApp.models.dtos.ClientData;
+import jwzp.wp.VetApp.models.records.ClientRecord;
+import jwzp.wp.VetApp.resources.ClientsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceTest {
+
+    @Mock
+    ClientsRepository clientsRepository;
+
+    @Test
+    public void testAddClient() throws Exception {
+        ClientData requested = new ClientData("Marcus", "Aurelius");
+        ClientRecord expected = new ClientRecord(1, "Marcus", "Aurelius");
+        Mockito.when(clientsRepository.save(Mockito.any())).thenReturn(expected);
+
+        var uut = new ClientsService(clientsRepository);
+        var result = uut.addClient(requested);
+
+        assert result.get().equals(expected);
+    }
+
+}
