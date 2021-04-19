2257d1ef81218b32a4b0da5a5bd6d20ce9fc76d2
wiktor.przadka.1 <wiktor.przadka.1@gmail.com>
2021-04-19 16:00:55 +0200

    test clients controller getters

---
+++ src/test/java/jwzp/wp/VetApp/controller/ClientsControllerTest.java
@@ -0,0 +1,95 @@
+package jwzp.wp.VetApp.controller;
+
+import jwzp.wp.VetApp.controller.api.ClientsController;
+import jwzp.wp.VetApp.controller.api.ResponseToHttp;
+import jwzp.wp.VetApp.models.records.ClientRecord;
+import jwzp.wp.VetApp.service.ClientsService;
+import jwzp.wp.VetApp.service.ResponseErrorMessage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientsControllerTest {
+
+    @Mock
+    ClientsService clientsService;
+
+    @Test
+    public void testGetAllClientsPositive() throws Exception {
+        List<ClientRecord> clients = List.of(
+                new ClientRecord(0,  "Emanuel", "Kant"),
+                new ClientRecord(1,  "Stephen", "King")
+        );
+        Mockito.when(clientsService.getAllClients()).thenReturn(clients);
+        var expected = ResponseEntity.ok(
+                CollectionModel.of(
+                        clients.stream()
+                        .map(c -> c.add(linkTo(ClientsController.class).slash(c.id).withSelfRel()))
+                        .collect(Collectors.toList())
+                ).add(linkTo(ClientsController.class).withSelfRel())
+        );
+        var uut = new ClientsController(clientsService);
+
+        var result = uut.getAllClients();
+
+        assert result.equals(expected);
+        Mockito.verify(clientsService, Mockito.timeout(1)).getAllClients();
+    }
+
+    @Test
+    public void testGetAllClientsEmpty() throws Exception {
+        List<ClientRecord> noClients = Collections.emptyList();
+        Mockito.when(clientsService.getAllClients()).thenReturn(noClients);
+        var expected = ResponseEntity.ok(
+                CollectionModel.empty().add(linkTo(ClientsController.class).withSelfRel())
+        );
+        var uut = new ClientsController(clientsService);
+
+        var result = uut.getAllClients();
+
+        assert result.equals(expected);
+        Mockito.verify(clientsService, Mockito.timeout(1)).getAllClients();
+    }
+
+    @Test
+    public void testGetClientPositive() throws Exception {
+        int request = 0;
+        ClientRecord client = new ClientRecord(request,  "Emanuel", "Kant");
+        Mockito.when(clientsService.getClient(request)).thenReturn(Optional.of(client));
+        var expected = ResponseEntity.ok(
+                client.add(linkTo(ClientsController.class).slash(client.id).withSelfRel())
+        );
+        var uut = new ClientsController(clientsService);
+
+        var result = uut.getClient(request);
+
+        assert result.equals(expected);
+        Mockito.verify(clientsService, Mockito.timeout(1)).getClient(request);
+    }
+
+    @Test
+    public void testGetClientClientNotFound() throws Exception {
+        int request = 0;
+        ClientRecord client = new ClientRecord(1,  "Emanuel", "Kant");
+        Mockito.when(clientsService.getClient(request)).thenReturn(Optional.empty());
+        var expected = ResponseToHttp.getFailureResponse(ResponseErrorMessage.CLIENT_NOT_FOUND);
+        var uut = new ClientsController(clientsService);
+
+        var result = uut.getClient(request);
+
+        assert result.equals(expected);
+        Mockito.verify(clientsService, Mockito.timeout(1)).getClient(request);
+    }
+}
