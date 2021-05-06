45f9fb8069872dc7a0f5d9db699903af498decba
pawelrys1 <paweku123@gmail.com>
2021-05-06 23:42:59 +0200

    update logsutils

--- src/main/java/jwzp/wp/VetApp/LogsUtils.java
+++ src/main/java/jwzp/wp/VetApp/LogsUtils.java
@@ -6,4 +6,15 @@ public class LogsUtils {
         return "Saved: " + saved.getClass() + " id: " + id;
     }
 
+    static public String logDeleted(Object saved, int id) {
+        return "Deleted: " + saved.getClass() + " id: " + id;
+    }
+
+    static public String logUpdated(Object saved, int id) {
+        return "Updated: " + saved.getClass() + " id: " + id;
+    }
+
+    static public String logWrongData(Object saved) {
+        return "Wrong data: " + saved.getClass();
+    }
 }
