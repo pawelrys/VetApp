6a68f9e410e33a9815c7dcbe99d316bcb965063c
Viking <wiktor.przadka.1@gmail.com>
2021-06-06 23:40:01 +0200

    create basic hashing logic for passwords

--- src/main/java/jwzp/wp/VetApp/models/records/UserRecord.java
+++ src/main/java/jwzp/wp/VetApp/models/records/UserRecord.java
@@ -1,29 +1,44 @@
 package jwzp.wp.VetApp.models.records;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity(name="users")
 public class UserRecord {
-    private String username;
-    private String hashedPassword;
+    @Id
+    private final int id;
+    private final String username;
+    private final String hashedPassword;
+    private final String salt;
 
-    public UserRecord() {}
+    public UserRecord(String username, String hashedPassword, String salt) {
+        this.id = -1;
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+        this.salt = salt;
+    }
 
-    public UserRecord(String username, String password) {
-        this.setUsername(username);
-        this.setHashedPassword(password);
+    public UserRecord() {
+        this.id = -1;
+        this.username = "";
+        this.hashedPassword = "";
+        this.salt = "";
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getHashedPassword() {
         return hashedPassword;
     }
 
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
+    public String getSalt() {
+        return salt;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- src/main/java/jwzp/wp/VetApp/service/Security/UsersService.java
+++ src/main/java/jwzp/wp/VetApp/service/Security/UsersService.java
@@ -1,4 +1,27 @@
 package jwzp.wp.VetApp.service.Security;
 
+import jwzp.wp.VetApp.models.records.UserRecord;
+import jwzp.wp.VetApp.resources.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository){
+        this.usersRepository = usersRepository;
+    }
+
+    private String hashPassword(String password, String salt){
+
+        return BCrypt.hashpw(password, salt);
+    }
+
+    private boolean isPasswordValid(String username, String password){
+        UserRecord user = usersRepository.findByName(username);
+        String hashedProvided = BCrypt.hashpw(password, user.getSalt());
+        return hashedProvided.equals(user.getHashedPassword());
+    }
 }
